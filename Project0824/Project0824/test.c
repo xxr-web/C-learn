c39c3c02784223d8200b2e57921131adcaaa8457
mengxin-xu <18842469642@163.com>
2023-08-24 23:53:40 +0800

    C Learn Day 30

---
+++ Project0824/Project0824/test.c
@@ -0,0 +1,75 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));
+//	return 0;
+//}
+//程序的结果是什么？
+
+//在X86环境下
+//假设结构体的⼤⼩是20个字节
+//程序输出的结构是啥？
+//struct Test
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p = (struct Test*)0x100000;
+//int main()
+//{
+//	printf("%p\n", p + 0x1);
+//	printf("%p\n", (unsigned long)p + 0x1);
+//	printf("%p\n", (unsigned int*)p + 0x1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	int* p = (int*)a;
+//	printf("%p\n", p);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x,%x", ptr1[-1], *ptr2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d", p[0]);
+//	return 0;
+//}
+
+//假设环境是x86环境，程序输出的结果是啥？
+//#include <stdio.h>
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	return 0;
+//}
+
+int main()
+{
+	int a = 16;
+	printf("%p\n", a);//就是补码转换而来
+	return 0;
+}
