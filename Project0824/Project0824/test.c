bc6376c1b19e0b7c588bd0edb66fced848d987e3
mengxin-xu <18842469642@163.com>
2023-08-25 23:49:53 +0800

    C Learn Day 31

--- Project0824/Project0824/test.c
+++ Project0824/Project0824/test.c
@@ -67,9 +67,19 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int a = 16;
+//	printf("%p\n", a);//就是补码转换而来
+//	return 0;
+//}
+
+#include <stdio.h>
 int main()
 {
-	int a = 16;
-	printf("%p\n", a);//就是补码转换而来
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int* ptr1 = (int*)(&aa + 1);
+	int* ptr2 = (int*)(*(aa + 1));
+	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
 	return 0;
 }
---
+++ Project0825/Project0825/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdio.h>
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//	return 0;
+//}
+//POINT
+//ER
+//ST
+//EW
+
+#include <stdio.h>
+int main()
+{
+	int a[4] = { 1, 2, 3, 4 };
+	int* ptr1 = (int*)(&a + 1);
+	int* ptr2 = (int*)((int)a + 1);
+	printf("%x,%x", ptr1[-1], *ptr2);
+	return 0;
+}
