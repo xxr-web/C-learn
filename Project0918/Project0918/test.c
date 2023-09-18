1a0de89ef142093be084517a3376e44836d2b78e
mengxin-xu <18842469642@163.com>
2023-09-18 11:29:05 +0800

    C Learn Day NEW

---
+++ Project0918/Project0918/test.c
@@ -0,0 +1,206 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+//int main()
+//{
+//	//字符分类函数,判断字符的类别
+//	int ret = islower('q');
+//	int ret1 = islower('Q');
+//	printf("%d\n", ret);
+//	printf("%d\n", ret1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char ch = '0';
+//	ch = getchar();
+//	if (islower(ch) != 0)
+//		printf("%d\n", 1);
+//	else
+//		printf("%d\n", 0);
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	char str[] = "i am a student.";
+//	size_t sz = strlen(str);
+//	for (int i = 0; i < (int)sz; i++)
+//	{
+//		if (islower(str[i]) != 0)
+//		{
+//			//str[i] = str[i] - 32;
+//			str[i] -= 32;
+//		}
+//	}
+//	puts(str);
+//	printf("%s\n", str);
+//	return 0;
+//}
+
+//2.字符转换函数
+//int main()
+//{
+//	char str[] = "i am a student.";
+//	size_t sz = strlen(str);
+//	for (int i = 0; i < (int)sz; i++)
+//	{
+//		if (islower(str[i]) != 0)
+//		{
+//			//str[i] = str[i] - 32;
+//			//str[i] -= 32;
+//			str[i] = toupper(str[i]);
+//		}
+//	}
+//	puts(str);
+//	printf("%s\n", str);
+//	return 0;
+//}
+
+//3.strlen模拟实现
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	char* p = arr;
+//	int i = 0;
+//	while (*p != '\0')
+//	{
+//		i++;
+//		p++;
+//	}
+//	printf("%d\n", i);
+//	return 0;
+//}
+
+//typedef unsigned int ui;
+//int main()
+//{
+//	ui a = 3;
+//	ui b = 6;
+//	ui c = a - b;//变成补码之后再参与计算
+//	printf("%u", c);
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	if (strlen("abc") - strlen("abcdef") < 0)//strlen的返回值是size_t，无符号整型
+//	{
+//		printf(">\n");
+//	}
+//	else
+//	{
+//		printf("<\n");
+//	}
+//	return 0;
+//}
+
+//#include <assert.h>
+//size_t my_strlen(const char* p)
+//{
+//	assert(p != NULL);
+//	if (*p == '\0')
+//	{
+//		return 0;
+//	}
+//	else
+//		return 1 + my_strlen(p + 1);
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	size_t sz = my_strlen(arr);
+//	printf("%zd\n", sz);
+//	return 0;
+//}
+
+//#include <assert.h>
+//size_t my_strlen(const char* p)
+//{
+//	assert(p != NULL);
+//	const char* s = p;
+//	while (*s != '\0')
+//	{
+//		s++;
+//	}
+//	return s - p;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	size_t sz = my_strlen(arr);
+//	printf("%zd\n", sz);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char arr1[] = "Hello bit !";
+//	char arr2[] = "##############";
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);//拷贝了原字符串的\0，并且在此截止
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	char arr1[4] = "abc";
+//	//char* p = "abcdef";
+//	char arr2[] = "abcdef";
+//	//strcpy(p, arr1);
+//	//printf("%s\n", p);
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//void my_strcpy(char* p, char* q)
+//{
+//	size_t sz = strlen(q);
+//	for (int i = 0; i <= sz; i++)
+//	{
+//		*(p + i) = *(q + i);
+//	}
+//}
+//int main()
+//{
+//	char arr1[] = "abcdef";
+//	char arr2[20] = "0";
+//	my_strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+#include <assert.h>
+char* my_strcpy(char* dest, const char* src)//源头不改
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);//优化
+
+	while ((*dest++ = *src++))//'\0'的ASCII码值就是0，结束循环，里面是一个赋值表达式，判断的是*dest++
+	{
+		;
+	}
+	return ret;
+}
+
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[20] = "0";
+	char* p = my_strcpy(arr2, arr1);
+	printf("%s\n", p);
+	return 0;
+}
+
+
+
+
+
+ 
--- Project字符指针特殊用法/Project0826/test.c
+++ Project字符指针特殊用法/Project0826/test.c
@@ -1,17 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+//int main()
+//{
+//	char* arr[3] = { "Jlu", "University", "Love" };
+//	printf("%s\n", arr[0]);
+//	printf("%s\n", *(arr + 1));//解引用才是字符指针
+//	printf("%p\n", arr);
+//	printf("%p\n", (arr + 1));
+//	printf("%s\n", arr + 1);//这个字符指针的地址
+//	char arr1[4] = { "abc" };
+//	printf("%p\n", arr1);
+//	printf("%p\n", arr1 + 1);
+//	char* p = "abc";
+//	printf("%s\n", p);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* p = "abc";
+//	printf("%s\n", p);
+//	printf("%c\n", *p);
+//	return 0;
+//}
+
+#include <stdio.h>
 int main()
 {
-	char* arr[3] = { "Jlu", "University", "Love" };
-	printf("%s\n", arr[0]);
-	printf("%s\n", *(arr + 1));//解引用才是字符指针
-	printf("%p\n", arr);
-	printf("%p\n", (arr + 1));
-	printf("%s\n", arr + 1);//这个字符指针的地址
-	char arr1[4] = { "abc" };
-	printf("%p\n", arr1);
-	printf("%p\n", arr1 + 1);
-	char* p = "abc";
-	printf("%s\n", p);
+	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+	char** cp[] = { c + 3,c + 2,c + 1,c };
+	char*** cpp = cp;
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
 	return 0;
 }
